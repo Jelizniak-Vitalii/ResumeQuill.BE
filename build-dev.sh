3d08df6a7d7e1bec3637dd6c87c710225be3912a
Zhelizniak Vitalii <v.jelizniak@gmail.com>
2025-11-14 17:32:13 +0200

    Remove unused `container_name` property from production Docker Compose file.

--- build-dev.sh
+++ build-dev.sh
@@ -3,6 +3,6 @@ set -e
 
 git pull
 
-docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
+docker compose -f docker-compose.dev.yml up -d --build
 
 docker image prune -f
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 git pull
 
-docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
+docker compose -f docker-compose.prod.yml up -d --build
 
 # опционально: если хочешь еще и override (локальные тайны)
 # docker compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.override.yml up -d --build
