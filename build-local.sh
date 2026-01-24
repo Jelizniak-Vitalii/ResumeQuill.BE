1f0d115af1be3ea3664c704084c3aa6e3a9f8014
Zhelizniak Vitalii <v.jelizniak@gmail.com>
2026-01-24 11:34:39 +0200

    Remove local and dev Docker Compose configurations, consolidate shared settings into base/prod setups, and simplify Prometheus/Grafana volume management.

--- build-local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-docker compose -p resumequill-be-local --env-file .env.local -f docker-compose.yml -f docker-compose.local.yml up -d --build
-
-docker image prune -f --filter "until=168h"
