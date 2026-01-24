4ddfe74fac9e210c373a2168ac932e05556625cd
Zhelizniak Vitalii <v.jelizniak@gmail.com>
2026-01-24 11:19:56 +0200

    Refactor Docker Compose configurations and build scripts: modularize base/prod/dev setups, add volume creation logic, and optimize service definitions

--- build-dev.sh
+++ build-dev.sh
@@ -3,6 +3,11 @@ set -e
 
 git pull
 
-docker compose -p resumequill-be-dev --env-file .env.dev -f docker-compose.yml -f docker-compose.dev.yml up -d --build
+docker compose \
+  -p resumequill-be-dev \
+  --env-file .env.dev \
+  -f docker-compose.base.yml \
+  -f docker-compose.dev.yml \
+  up -d --build
 
 docker image prune -f --filter "until=168h"
--- build.sh
+++ build.sh
@@ -3,6 +3,15 @@ set -e
 
 git pull
 
-docker compose -p resumequill-be-prod up -d --build
+for v in pgdata_prod grafana_data_prod loki_data_prod prometheus_data_prod; do
+  docker volume inspect "$v" >/dev/null 2>&1 || docker volume create "$v"
+done
+
+docker compose \
+  -p resumequill-be-prod \
+  --env-file .env \
+  -f docker-compose.base.yml \
+  -f docker-compose.prod.yml \
+  up -d --build
 
 docker image prune -f --filter "until=168h"
