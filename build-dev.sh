ea063b9a767afde5fe5e7d22706cbb6f19148bb8
Zhelizniak Vitalii <v.jelizniak@gmail.com>
2026-01-24 11:06:58 +0200

    Restrict Docker image pruning to images older than 7 days and expose Grafana on localhost:3000 in Docker Compose configuration

--- build-dev.sh
+++ build-dev.sh
@@ -5,4 +5,4 @@ git pull
 
 docker compose -p resumequill-be-dev --env-file .env.dev -f docker-compose.yml -f docker-compose.dev.yml up -d --build
 
-docker image prune -f
+docker image prune -f --filter "until=168h"
--- build-local.sh
+++ build-local.sh
@@ -3,4 +3,4 @@ set -e
 
 docker compose -p resumequill-be-local --env-file .env.local -f docker-compose.yml -f docker-compose.local.yml up -d --build
 
-docker image prune -f
+docker image prune -f --filter "until=168h"
